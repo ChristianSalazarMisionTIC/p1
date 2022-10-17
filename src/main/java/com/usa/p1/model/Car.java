5e25a011b2b86b84559fbf7d4aaeef58aa08b8b4
RobertoAnguloDeveloper <rangulot@outlook.com>
2022-10-17 08:51:25 -0500

    Test commit-2

--- src/main/java/com/usa/p1/model/Car.java
+++ src/main/java/com/usa/p1/model/Car.java
@@ -25,7 +25,7 @@ public class Car {
 
     @ManyToOne
     @JoinColumn(name = "idGama")
-    @JsonIgnoreProperties("libs")
+    @JsonIgnoreProperties("cars")
     private Gama gama;
 
     @OneToMany(cascade = {CascadeType.PERSIST},mappedBy = "car")
